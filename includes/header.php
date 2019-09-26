88457d4fb3743b744e8848edcd8c52ed01995d99
YSSVR <55809947+YSSVR@users.noreply.github.com>
2019-09-26 06:38:14 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@ we link this header.php file to all other webpages using 'include' command
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>                        
             </button>
-            <a class="navbar-brand" href="index.html">Lifestyle Store</a>
+            <a class="navbar-brand" href="index.php">Lifestyle Store</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
