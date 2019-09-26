0dd4b0cdcefeb98baad907d973835a5205a90243
YSSVR <55809947+YSSVR@users.noreply.github.com>
2019-09-26 06:24:47 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@ we link this footer.php file to all other webpages using 'include' command-->
 <footer>
     <div class="container">
         <center>
-            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000<br>This Website is Developed by Yaramala.Siva Sai Vardhan Reddy</p>	
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</br>This Website is Developed by Yaramala.Siva Sai Vardhan Reddy</p>	
         </center>
     </div>
 </footer>
